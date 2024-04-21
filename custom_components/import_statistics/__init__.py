b704abfbb767f7251553ad48cdb65ec65beaea17
klausj1 <jandl.klaus@gmx.at>
2024-04-21 18:27:18 +0000

    feat: playing around with hass-methods regarding state and history

--- custom_components/import_statistics/__init__.py
+++ custom_components/import_statistics/__init__.py
@@ -3,10 +3,12 @@
 from datetime import datetime
 import logging
 import os
+import time
 import zoneinfo
 
 import pandas as pd
 
+from homeassistant.components.recorder.history import state_changes_during_period
 from homeassistant.components.recorder.statistics import (
     async_add_external_statistics,
     async_import_statistics,
@@ -38,6 +40,29 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool: # pylint: disable=un
         file_path = f"{hass.config.config_dir}/{call.data.get(ATTR_FILENAME)}"
 
         hass.states.set("import_statistics.import_from_file", file_path)
+
+# This can be used to check if an entity exists
+        entity_id = "sensor.sun_solar_azimuth"
+        x: hass.State
+
+        x = hass.states.get(entity_id)
+        _LOGGER.debug(f"State of {entity_id}: {x}")
+        if x is not None:
+            _LOGGER.debug("x != None")
+        _LOGGER.debug(f"State of {entity_id}: {x.state}")
+        _LOGGER.debug(f"Unit of {entity_id}: {x.attributes['unit_of_measurement']}")
+
+        datetime_str = '09/19/22 13:55:26'
+
+        datetime_object = datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S').replace(tzinfo=zoneinfo.ZoneInfo("Europe/Vienna"))
+
+# This can be used to get the first value of an entity in the history
+        _LOGGER.debug("Start query")
+        z = hass.components.recorder.get_instance(hass).async_add_executor_job(state_changes_during_period, hass, datetime_object, None, entity_id, False, False, 1)
+        # z is a future
+        while not z.done():
+            time.sleep(0.001)
+        _LOGGER.debug(f"History of {entity_id}: {z.result()}")
 
         stats = _prepare_data_to_import(file_path, call)
 
--- custom_components/import_statistics/todo.md
+++ custom_components/import_statistics/todo.md
@@ -1,9 +1,19 @@
 # ToDo
 
-- Check if HACS shows version 1.0
-
-## Later
-
 - Distribute code to different files
-- Provide date string like %d.%m.%Y %H:%M as parameter
+    - just work
+- Fix bug with seconds in timestamp (only support minutes, and add tests for some “crazy” timestamp)
+- Provide date string like %d.%m.%Y %H:%M as parameter (Allow different dateTime-formats)
+    - just work
 - Check if entity with '.' exists
+    - found out how to do this: use hass.states.get("sensor.sun_next_dawn"); this also returns the unit
+- Take the unit from existing entity
+    - see above, hass.states.get
+- Calculate sum automatically, based on the oldest existing value
+    - last value of the history can be read with z = hass.components.recorder.get_instance(hass).async_add_executor_job(state_changes_during_period, hass, datetime_object, None, "sensor.sun_solar_azimuth", False, False, 1)
+
+# Later
+
+- Export data
+    - Use a second service
+        - also with reading the history
